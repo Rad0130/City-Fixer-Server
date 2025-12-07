f78ca52160d3b91cae70fe494b1e2a918606e855
Your Name <your.email@example.com>
2025-12-08 04:34:40 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app=express()
 const port= process.env.PORT || 3000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.dm5zycv.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dm5zycv.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
